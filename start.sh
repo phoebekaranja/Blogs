59f04745916db798f6f051d015ee46c69794f12a
phoebekaranja <phoebemurish@gmail.com>
2018-10-21 21:41:09 +0300

    adding some changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME=phoebemurish@gmail.com
 export MAIL_PASSWORD=luckyhope
 export SECRET_KEY=phoebe
-export DATABASE_URL='postgres://fmnjtlfdxhxwqw:4d5fa1b37d49f52ca4ef81e4c09654d449de60db5646fe90d9d1c886b96e7bc9@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d8urob8lnudtol'
+export DATABASE_URL='postgresql+psycopg2://pheobe:pheobe@localhost/blog'
 python3.6 manage.py server
